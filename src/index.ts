1eb7140df5b0292a3a3b91c5ac7de75912022014
saif hany <saifhany898@gmail.com>
2022-01-28 23:12:08 +0200

    add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello saif")
+import express ,{Application,Request,Response} from 'express';
+
+
+const PORT=3000
+// create instance server
+const app:Application = express();
+
+app.get('/',(req:Request, res:Response)=>{
+    res.json({
+        message:'hello'
+    })
+})
+// start express server
+app.listen(PORT,()=>{
+    console.log(`Server is starting at port:${PORT}`)
+});
+
+export default app;
