4bf97622f8f94fc1ce1ec795abbb5c36e52312be
saif hany <saifhany898@gmail.com>
2022-03-12 00:32:42 +0200

    :necktie: add more code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
-import express ,{Application,Request,Response} from 'express';
-
-
-const PORT=3000
+import express ,{Application,json,Request,Response} from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
+import errorMiddleware from './middleware/error.middleware';
+import config from './config';
+import routes from './routes';
+const PORT = config.port || 3000;
 // create instance server
 const app:Application = express();
+// HTTP request logger middleware
+app.use(morgan('comman'));
+// HTTP security middleware
+app.use(helmet());
+// middleware parse incoming request
+app.use(express.json());
+// apply the rate limiting middleware to all requests
+app.use(
+    RateLimit({
+        windowMs: 60 * 60 * 1000, // 15 minutes
+        max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        message: 'el3b b3ed ya ro7 mama',
+    })
+)
+app.use('/api',routes);  
 
 app.get('/',(req:Request, res:Response)=>{
     res.json({
-        message:'hello'
+        message:'hello World! ✔😊',
+    })
+})
+
+
+app.use(errorMiddleware);
+app.use((_req:Request, res:Response)=>{
+    res.status(404).json({
+        message: 'ohh you are lost,read the API documentation to find your way back home 😂🤷‍♀️'
     })
 })
 // start express server
 app.listen(PORT,()=>{
-    console.log(`Server is starting at port:${PORT}`)
+    console.log(`Server is starting at port:${PORT} 🥰🔥`)
 });
 
 export default app;
