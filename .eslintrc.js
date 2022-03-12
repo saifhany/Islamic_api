7230594e52c8576f87204214f5e2a2919dabb574
saif hany <saifhany898@gmail.com>
2022-03-12 02:07:10 +0200

    hash

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "rules": {
-        "prettier/prettier": 2,
-        quotes:['error','single'],
-        "no-console":0,
-        "no-var":"error",
-        "prefer-const":"error"
-    },
-}
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true,
+//         "node": true,
+//     },
+//     "extends": [
+//         "eslint:recommended",
+//         "plugin:@typescript-eslint/recommended",
+//         "prettier"
+//     ],
+//     "parser": "@typescript-eslint/parser",
+//     "parserOptions": {
+//         "ecmaVersion": "latest",
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "@typescript-eslint",
+//         "prettier"
+//     ],
+//     "rules": {
+//         "prettier/prettier": 2,
+//         quotes:['error','single'],
+//         "no-console":0,
+//         "no-var":"error",
+//         "prefer-const":"error"
+//     },
+// }
