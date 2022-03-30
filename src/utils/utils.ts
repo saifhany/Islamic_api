62536303946c2747149f7e1eccc2cff8f429f7ee
saif hany <saifhany898@gmail.com>
2022-03-30 02:06:32 +0200

    Add shedule node-cron -add shedule DeleteAllPost everyNight

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,10 @@
 import config from "../config";
 import bcrypt from "bcrypt";
+import { PrismaClient } from '@prisma/client'
+import cron  from 'node-cron';
+
+
+const prisma = new PrismaClient()
 
 
 export const hashPassword = (password: string) => {
@@ -10,4 +15,21 @@ export const hashPassword = (password: string) => {
 export const comparePassword = (password:string, hashed:any) => {
     return bcrypt.compareSync( `${password}${config.pepper}`, hashed);
   };
-  
+ 
+//   @ts-ignore
+
+
+function recurring() {  
+    cron.schedule('59 23 * * *', async () => {     
+         await prisma.post.deleteMany({})
+    });
+}
+
+function ff() {  
+    cron.schedule('* * * * *', function() {
+        console.log('running a task every minute');
+      });
+}
+
+ff();
+recurring();
